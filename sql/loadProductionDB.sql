27f981229761677325422c7ae46b44eba39de522
PeNg <89747673+PeNgzzzzz@users.noreply.github.com>
2023-07-11 10:54:46 -0400

    test-production

--- sql/loadProductionDB.sql
+++ sql/loadProductionDB.sql
@@ -1,6 +1,7 @@
-LOAD DATA INFILE 'data.csv'
+set global local_infile=true;
+LOAD DATA LOCAL INFILE '/Users/wilson/Documents/GitHub/CS-348/sql/data.csv'
 INTO TABLE Musics
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS
 (@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9, @col10, @col11, @col12, @col13, @col14, @col15, @col16, @col17, @col18, @col19, @col20, @col21, @col22, @col23, @col24, @col25, @col26, @col27, @col28, @col29, @col30, @col31)
-SET aNmame = @col2, mNmame = @col3, rDate = @col4, genre = @col5, lyrics = @col6, topic = @col30;
+SET aName = @col2, mName = @col3, rDate = @col4, genre = @col5, lyrics = @col6, topic = @col30;
--- sql/test-production.sql
+++ sql/test-production.sql
@@ -13,16 +13,22 @@ INSERT INTO Reviews (userName, mID, rating, comment, dt) VALUES ('wwan', 1, 5, '
 SELECT * FROM Reviews;
 
 -- Show all reviews of a user from most recent to lease recent
+INSERT INTO Reviews (userName, mID, rating, comment, dt) VALUES ('wwan', 2, 4, 'not bad!', '2023-07-09');
 SELECT * FROM Reviews WHERE userName LIKE 'wwan' order by dt DESC;
 
 -- Show all reviews of a song from most recent to lease recent
+INSERT INTO Reviews (userName, mID, rating, comment, dt) VALUES ('wwan', 1, 3, 'ok!', '2023-07-08');
+INSERT INTO Reviews (userName, mID, rating, comment, dt) VALUES ('wwan', 1, 0, 'bad!', '2023-07-09');
 SELECT * FROM Reviews WHERE mID = 1 order by dt DESC;
 
 -- Show top five songs by users' reviews (top five in average ratings)
+INSERT INTO Reviews (userName, mID, rating, comment, dt) VALUES ('wwan', 3, 5, 'love it!', curdate());
+INSERT INTO Reviews (userName, mID, rating, comment, dt) VALUES ('wwan', 4, 0, 'hate it!', curdate());
+INSERT INTO Reviews (userName, mID, rating, comment, dt) VALUES ('wwan', 5, 2, 'dislike!', curdate());
 SELECT mName, aName, genre FROM Reviews NATURAL JOIN Musics GROUP BY mID ORDER BY AVG(rating) DESC LIMIT 5;
 
 -- The favorite actist, song and genre of a user (based on their rating)
-SELECT mName, aName, genre, AVG(rating) as rating FROM Reviews NATURAL JOIN Users NATURAL JOIN Musics WHERE userName LIKE 'wwan' GROUP BY mID HAVING AVG(rating) > 3 ORDER BY AVG(rating) DESC;
+SELECT mName, aName, genre, AVG(rating) as rating FROM Reviews NATURAL JOIN Users NATURAL JOIN Musics WHERE userName LIKE 'wwan' GROUP BY mID HAVING AVG(rating) > 3 ORDER BY AVG(rating) DESC LIMIT 1;
 
 -- Assume user with username testUser1 deletes his account, so his reviews will be deleted as well
 DELETE FROM Users WHERE userName LIKE 'wwan';
@@ -31,6 +37,6 @@ SELECT * FROM Reviews;
 
 -- Recommend songs by users' reviews
 -- If he likes blues
-SELECT mName, aName, genre FROM Musics WHERE genre LIKE 'blues';
+SELECT mName, aName, genre FROM Musics WHERE genre LIKE 'blues' LIMIT 5;
 -- If he does not like blues
-SELECT mName, aName, genre FROM Musics WHERE genre NOT LIKE 'blues';
+SELECT mName, aName, genre FROM Musics WHERE genre NOT LIKE 'blues' LIMIT 5;
